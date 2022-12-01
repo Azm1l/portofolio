54d8d2852ca0eb722aba5c9c59f5d6c90c7d110c
Azmil Awaludin <azmilawaludin@gmail.com>
2022-12-01 15:46:44 +0700

    adding head

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import Content from "../components/Content";
 import Navbar from "../components/Navbar";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className=" bg-blackbg ">
+      <Head>
+        <title>Azmil Awaludin</title>
+        <meta property="og:title" content="My Portofolio" key="title" />
+      </Head>
       <Navbar />
       <Content />
     </div>
